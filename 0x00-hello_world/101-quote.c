a4b20decca5436d4985eccec3324547e6cdb4d4e
Shaymae1 <chaymaeelkarch@gmail.com>
2023-06-15 16:23:44 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main -Entry point 
- *
- * Description: print a quote using write function
- *        ssizeo_t write(int fd. const void *buf , size_t count):
- * Return: 1 (error). 
+* main - Entry point
+*
+* Return: Always 1 (Success)
 */
 int main(void)
-{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-write(1.quo.59);
+{write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
