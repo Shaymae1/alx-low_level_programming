4a384055f9f8e72f13a921a0b4722566d0a45494
Shaymae1 <chaymaeelkarch@gmail.com>
2023-06-20 14:23:34 -0700

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,7 +5,7 @@
  *               0 if n is zero and - if n is less
  *               then zero.
  *
- * @c: takes integer type input for function.
+ * @n: takes integer type input for function.
  *
  * Return: 1 if +, 0 if 0 and -1 if -
 */
