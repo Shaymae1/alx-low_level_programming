bff695ce2e751844629d779de5803f6758b5ffa6
Shaymae1 <chaymaeelkarch@gmail.com>
2023-06-20 15:23:58 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -31,7 +31,7 @@ void times_table(void)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + 48); /*get the first digit*/
-			_putchar((prod / 10) + 48); /*get the second digit*/
+			_putchar((prod % 10) + 48); /*get the second digit*/
 		}
 	_putchar('\n');
 	}
