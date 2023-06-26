63b31b92f652cdb9f62630807531c76b0506dcd6
Shaymae1 <chaymaeelkarcha@gmail.com>
2023-06-26 11:21:53 -0700

    finish?

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,16 +9,25 @@
 
 int main(void)
 {
-	int sum;
-	char c;
+	int pass[100];
+	int i, sum, n;
+
+	sum = 0;
 
 	srand(time(NULL));
-	while (sum <= 2645)
+	for (i = 0; i < 100; i++)
 	{
-		c = rand() % 120;
-		sum += c;
-		putchar(c);
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
 	}
-	putchar(2772 - sum);
+
 	return (0);
 }
