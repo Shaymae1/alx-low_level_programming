acbc8b20f9734449ab9fd75a88643df5be8173e9
Shaymae1 <chaymaeelkarcha@gmail.com>
2023-07-07 12:40:18 -0700

    hi

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,10 @@
+#ifdef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _atoi(char *s);
+int main(int argc_attribute_((unused)), char *argv[]);
+int main(int argc, char *argv[]);
+int check_num(char *str);
+
+#endif
