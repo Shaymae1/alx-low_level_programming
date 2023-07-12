52f06ebe21dfd92d1139d187f4727462fa96b659
Shaymae1 <chaymaeelkarcha@gmail.com>
2023-07-11 17:08:36 -0700

    salka

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,21 +12,19 @@ char *str_concat(char *s1, char *s2)
 {
 	char *conct;
 	int i, ci;
-	
+
 	if (s1 == NULL)
-		 s1 = "";
+		s1 = "";
 	if (s2 == NULL)
-		 s2 = "";
-	
+		s2 = "";
 	i = ci = 0;
 	while (s1[i] != '\0')
-		  i++;
+		i++;
 	while (s2[ci] != '\0')
-		  ci++;
+		ci++;
 	conct = malloc(sizeof(char) * (i + ci + 1));
-	
 	if (conct == NULL)
-		 return (NULL);
+		return (NULL);
 	i = ci = 0;
 	while (s1[i] != '\0')
 	{
