034dc3d571fac713cce99b6df3e69088b6ca95b9
Shaymae1 <Chaymaeelkarcha@gmail.com>
2023-08-07 08:30:33 -0700

    nayf

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,5 +1,5 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
 
 #include <unistd.h>
 #include <sys/types.h>
@@ -7,7 +7,6 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <elf.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
